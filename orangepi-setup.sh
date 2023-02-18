e9460990cf013d745dd4c87a785c189634384f25
Inspector Soviet <www.alex.avil@mail.ru>
2023-02-18 15:54:26 +0400

    apt-fast

--- orangepi-setup.sh
+++ orangepi-setup.sh
@@ -5,4 +5,5 @@ sudo apt install armbian-config -y
 sudo apt autoremove -y
 curl -fsSL https://deb.nodesource.com/setup_19.x | sudo -E bash - &&\
 sudo apt-get install -y nodejs
+/bin/bash -c "$(curl -sL https://git.io/vokNn)"
 sudo reboot
