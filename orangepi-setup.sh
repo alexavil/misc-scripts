0709e5830c96bb1bf1ef3bbf748117d4e07ec132
Inspector Soviet <www.alex.avil@mail.ru>
2023-02-18 15:56:33 +0400

    git

--- orangepi-setup.sh
+++ orangepi-setup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 sudo apt update
 sudo apt upgrade -y
-sudo apt install armbian-config -y
+sudo apt install armbian-config git -y
 sudo apt autoremove -y
 curl -fsSL https://deb.nodesource.com/setup_19.x | sudo -E bash - &&\
 sudo apt-get install -y nodejs
