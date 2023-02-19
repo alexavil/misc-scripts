ea468abc8be38d9f7d43358dc735715050bfdece
Inspector Soviet <www.alex.avil@mail.ru>
2023-02-19 11:59:32 +0400

    split the nextcloud script

---
+++ opi-docker-setup.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+sudo apt update
+sudo apt upgrade -y
+sudo apt install apparmor apparmor-utils apparmor-profiles* -y
+curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh get-docker.sh
+sudo usermod -aG docker $USER
--- opi-nextcloud-setup.sh
+++ opi-nextcloud-setup.sh
@@ -1,9 +1,3 @@
 #!/bin/sh
-sudo apt update
-sudo apt upgrade -y
-curl -fsSL https://get.docker.com -o get-docker.sh
-sudo sh get-docker.sh
-sudo usermod -aG docker $USER
-sudo apt install apparmor apparmor-utils apparmor-profiles* -y
 docker pull ownyourbits/nextcloudpi
 docker run -d -p 4443:4443 -p 443:443 -p 80:80 -v ncdata:/data --name nextcloudpi ownyourbits/nextcloudpi $1
