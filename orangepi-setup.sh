15c0b98c3e1046e24b3eebf72b552f109ae147fc
Inspector Soviet <www.alex.avil@mail.ru>
2023-04-07 12:49:35 +0400

    add alias

--- orangepi-setup.sh
+++ orangepi-setup.sh
@@ -6,4 +6,4 @@ sudo apt autoremove -y
 curl -fsSL https://deb.nodesource.com/setup_19.x | sudo -E bash - &&\
 sudo apt-get install -y nodejs
 /bin/bash -c "$(curl -sL https://git.io/vokNn)"
-sudo reboot
+alias cls=clear
