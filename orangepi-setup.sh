d47e3784b4f09318a96e76f008316c8450c36f2f
alexavil <www.alex.avil@mail.ru>
2023-08-18 21:46:08 +0400

    update node script

--- orangepi-setup.sh
+++ orangepi-setup.sh
@@ -3,6 +3,6 @@ sudo apt update
 sudo apt upgrade -y
 sudo apt install armbian-config git man-db -y
 sudo apt autoremove -y
-curl -fsSL https://deb.nodesource.com/setup_19.x | sudo -E bash - &&\
+curl -fsSL https://deb.nodesource.com/setup_20.x | sudo -E bash - &&\
 sudo apt-get install -y nodejs
 /bin/bash -c "$(curl -sL https://git.io/vokNn)"
